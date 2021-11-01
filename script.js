35a3c8f1b04be85b6f9e63613894ca6d44840f6f
manishaasadasivan <89304644+manishaasadasivan@users.noreply.github.com>
2021-11-01 20:32:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+    document.querySelector(".container").innerHTML=`<h2>10</h2>`
+    setTimeout(() =>{
+        document.querySelector(".container").innerHTML=`<h2>9</h2>`
+        setTimeout(() =>{
+            document.querySelector(".container").innerHTML=`<h2>8</h2>`
+            setTimeout(() =>{
+                document.querySelector(".container").innerHTML=`<h2>7</h2>`
+                setTimeout(() =>{
+                    document.querySelector(".container").innerHTML=`<h2>6</h2>`
+                    setTimeout(() =>{
+                        document.querySelector(".container").innerHTML=`<h2>5</h2>`
+                        setTimeout(() =>{
+                            document.querySelector(".container").innerHTML=`<h2>4</h2>`
+                            setTimeout(() =>{
+                                document.querySelector(".container").innerHTML=`<h2>3</h2>`
+                                setTimeout(() =>{
+                                    document.querySelector(".container").innerHTML=`<h2>2</h2>`
+                                    setTimeout(() =>{
+                                        document.querySelector(".container").innerHTML=`<h2>1</h2>`
+                                        setTimeout(() =>{
+                                            document.querySelector(".container").innerHTML=`<h2>0</h2>`
+                                            setTimeout(() =>{
+                                                document.querySelector(".container").innerHTML=`<h2> Happy Independence Day</h2>`
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+}, 1000);
